57dd5a1d41f94b8535ad374c6da985b5fea7f22c
yyue <yy@yyue.dev>
2021-04-14 10:20:25 +0000

    proto

--- src/proto/news/NewsServiceClientPb.ts
+++ src/proto/news/NewsServiceClientPb.ts
@@ -1,5 +1,5 @@
 /**
- * @fileoverview gRPC-Web generated client stub for empty
+ * @fileoverview gRPC-Web generated client stub for news
  * @enhanceable
  * @public
  */
@@ -16,6 +16,145 @@ import * as grpcWeb from 'grpc-web';
 import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb';
 import * as news_news_pb from '../news/news_pb';
 
+
+export class NewsClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: any; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: any; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoNews = new grpcWeb.AbstractClientBase.MethodInfo(
+    news_news_pb.NewsItem,
+    (request: google_protobuf_empty_pb.Empty) => {
+      return request.serializeBinary();
+    },
+    news_news_pb.NewsItem.deserializeBinary
+  );
+
+  news(
+    request: google_protobuf_empty_pb.Empty,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/news.News/News',
+      request,
+      metadata || {},
+      this.methodInfoNews);
+  }
+
+  methodInfoNewsList = new grpcWeb.AbstractClientBase.MethodInfo(
+    news_news_pb.NewsResponse,
+    (request: google_protobuf_empty_pb.Empty) => {
+      return request.serializeBinary();
+    },
+    news_news_pb.NewsResponse.deserializeBinary
+  );
+
+  newsList(
+    request: google_protobuf_empty_pb.Empty,
+    metadata: grpcWeb.Metadata | null): Promise<news_news_pb.NewsResponse>;
+
+  newsList(
+    request: google_protobuf_empty_pb.Empty,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: news_news_pb.NewsResponse) => void): grpcWeb.ClientReadableStream<news_news_pb.NewsResponse>;
+
+  newsList(
+    request: google_protobuf_empty_pb.Empty,
+    metadata: grpcWeb.Metadata | null,
+    callback?: (err: grpcWeb.Error,
+               response: news_news_pb.NewsResponse) => void) {
+    if (callback !== undefined) {
+      return this.client_.rpcCall(
+        this.hostname_ +
+          '/news.News/NewsList',
+        request,
+        metadata || {},
+        this.methodInfoNewsList,
+        callback);
+    }
+    return this.client_.unaryCall(
+    this.hostname_ +
+      '/news.News/NewsList',
+    request,
+    metadata || {},
+    this.methodInfoNewsList);
+  }
+
+  methodInfoCall = new grpcWeb.AbstractClientBase.MethodInfo(
+    news_news_pb.Response,
+    (request: news_news_pb.Request) => {
+      return request.serializeBinary();
+    },
+    news_news_pb.Response.deserializeBinary
+  );
+
+  call(
+    request: news_news_pb.Request,
+    metadata: grpcWeb.Metadata | null): Promise<news_news_pb.Response>;
+
+  call(
+    request: news_news_pb.Request,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: news_news_pb.Response) => void): grpcWeb.ClientReadableStream<news_news_pb.Response>;
+
+  call(
+    request: news_news_pb.Request,
+    metadata: grpcWeb.Metadata | null,
+    callback?: (err: grpcWeb.Error,
+               response: news_news_pb.Response) => void) {
+    if (callback !== undefined) {
+      return this.client_.rpcCall(
+        this.hostname_ +
+          '/news.News/Call',
+        request,
+        metadata || {},
+        this.methodInfoCall,
+        callback);
+    }
+    return this.client_.unaryCall(
+    this.hostname_ +
+      '/news.News/Call',
+    request,
+    metadata || {},
+    this.methodInfoCall);
+  }
+
+  methodInfoStream = new grpcWeb.AbstractClientBase.MethodInfo(
+    news_news_pb.StreamingResponse,
+    (request: news_news_pb.StreamingRequest) => {
+      return request.serializeBinary();
+    },
+    news_news_pb.StreamingResponse.deserializeBinary
+  );
+
+  stream(
+    request: news_news_pb.StreamingRequest,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/news.News/Stream',
+      request,
+      metadata || {},
+      this.methodInfoStream);
+  }
+
+}
 
 export class EmptyClient {
   client_: grpcWeb.AbstractClientBase;
@@ -49,7 +188,7 @@ export class EmptyClient {
     metadata?: grpcWeb.Metadata) {
     return this.client_.serverStreaming(
       this.hostname_ +
-        '/empty.Empty/News',
+        '/news.Empty/News',
       request,
       metadata || {},
       this.methodInfoNews);
@@ -81,7 +220,7 @@ export class EmptyClient {
     if (callback !== undefined) {
       return this.client_.rpcCall(
         this.hostname_ +
-          '/empty.Empty/NewsList',
+          '/news.Empty/NewsList',
         request,
         metadata || {},
         this.methodInfoNewsList,
@@ -89,7 +228,7 @@ export class EmptyClient {
     }
     return this.client_.unaryCall(
     this.hostname_ +
-      '/empty.Empty/NewsList',
+      '/news.Empty/NewsList',
     request,
     metadata || {},
     this.methodInfoNewsList);
@@ -121,7 +260,7 @@ export class EmptyClient {
     if (callback !== undefined) {
       return this.client_.rpcCall(
         this.hostname_ +
-          '/empty.Empty/Call',
+          '/news.Empty/Call',
         request,
         metadata || {},
         this.methodInfoCall,
@@ -129,7 +268,7 @@ export class EmptyClient {
     }
     return this.client_.unaryCall(
     this.hostname_ +
-      '/empty.Empty/Call',
+      '/news.Empty/Call',
     request,
     metadata || {},
     this.methodInfoCall);
@@ -148,7 +287,7 @@ export class EmptyClient {
     metadata?: grpcWeb.Metadata) {
     return this.client_.serverStreaming(
       this.hostname_ +
-        '/empty.Empty/Stream',
+        '/news.Empty/Stream',
       request,
       metadata || {},
       this.methodInfoStream);
