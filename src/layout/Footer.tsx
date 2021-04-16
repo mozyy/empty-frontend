563c9ff1c1ea93ef114354c911f9ff76ea5721a9
yyue <yy@yyue.dev>
2021-04-16 09:46:55 +0000

    doc

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,37 +1,41 @@
 import * as React from 'react';
-import { experimentalStyled as styled, alpha } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import IconButton from '@material-ui/core/IconButton';
+// import Typography from '@material-ui/core/Typography';
+// import InputBase from '@material-ui/core/InputBase';
+// import Badge from '@material-ui/core/Badge';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Menu from '@material-ui/core/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import SearchIcon from '@material-ui/icons/Search';
+// import AccountCircle from '@material-ui/icons/AccountCircle';
+// import MailIcon from '@material-ui/icons/Mail';
+// import NotificationsIcon from '@material-ui/icons/Notifications';
+// import MoreIcon from '@material-ui/icons/MoreVert';
 import { Link } from '@material-ui/core';
 
 const StyledBox = styled(Box)({
-  textAlign: 'center'
+  textAlign: 'center',
   // backgroundColor: '#292929'
-})
+});
 
 const Footer:React.FC = () => {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
 
   return (
     <StyledBox>
-      © 2021-{year} {location.hostname} 版权所有
-      <br/>
+      © 2021-
+      {year}
+      {' '}
+      {window.location.hostname}
+      {' '}
+      版权所有
+      <br />
       <Link href="https://beian.miit.gov.cn" target="_blank" rel="noopener">蜀ICP备2021007564号-1</Link>
     </StyledBox>
   );
-}
+};
 
 export default Footer;
--- src/views/News/components/Item.tsx
+++ src/views/News/components/Item.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as grpcWeb from 'grpc-web';
 
 export interface ItemProps {}
 export const Item: React.FC<ItemProps> = (props) => {
