019b364a6210c79fce56f4df537b26efa2025cf1
yyue <yy@yyue.dev>
2021-04-21 11:57:27 +0000

    proto

--- src/proto/news/NewsServiceClientPb.ts
+++ src/proto/news/NewsServiceClientPb.ts
@@ -27,12 +27,52 @@ export class NewsClient {
                options?: null | { [index: string]: any; }) {
     if (!options) options = {};
     if (!credentials) credentials = {};
-    options['format'] = 'text';
+    options['format'] = 'binary';
 
     this.client_ = new grpcWeb.GrpcWebClientBase(options);
     this.hostname_ = hostname;
     this.credentials_ = credentials;
     this.options_ = options;
+  }
+
+  methodInfoHello = new grpcWeb.AbstractClientBase.MethodInfo(
+    news_news_pb.HelloResponse,
+    (request: news_news_pb.HelloRequest) => {
+      return request.serializeBinary();
+    },
+    news_news_pb.HelloResponse.deserializeBinary
+  );
+
+  hello(
+    request: news_news_pb.HelloRequest,
+    metadata: grpcWeb.Metadata | null): Promise<news_news_pb.HelloResponse>;
+
+  hello(
+    request: news_news_pb.HelloRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: news_news_pb.HelloResponse) => void): grpcWeb.ClientReadableStream<news_news_pb.HelloResponse>;
+
+  hello(
+    request: news_news_pb.HelloRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback?: (err: grpcWeb.Error,
+               response: news_news_pb.HelloResponse) => void) {
+    if (callback !== undefined) {
+      return this.client_.rpcCall(
+        this.hostname_ +
+          '/news.News/Hello',
+        request,
+        metadata || {},
+        this.methodInfoHello,
+        callback);
+    }
+    return this.client_.unaryCall(
+    this.hostname_ +
+      '/news.News/Hello',
+    request,
+    metadata || {},
+    this.methodInfoHello);
   }
 
   methodInfoNews = new grpcWeb.AbstractClientBase.MethodInfo(
